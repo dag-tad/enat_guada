9191cad11470952f0d5a6e60844f5c94ae0388ca
Dagi <dagtade@gmail.com>
2023-08-14 00:57:35 +0300

    add expandable text widget

--- lib/util/dimentions.dart
+++ lib/util/dimentions.dart
@@ -22,6 +22,7 @@ class Dimensions {
   static double width30 = screenHeight / 28.13;
   static double width45 = screenHeight / 18.75;
  
+  static double font16 = screenHeight / 52.7;
   static double font20 = screenHeight / 42.2;
   static double font26 = screenHeight / 32.46;
 
