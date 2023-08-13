dc2344dd86f8ea119427ce430847191253920951
Dagi <dagtade@gmail.com>
2023-08-13 22:35:08 +0300

    add popular food detail header and image

---
+++ lib/pages/food/popular_food_detail.dart
@@ -0,0 +1,41 @@
+import 'package:enat_guada/widgets/app_icon.dart';
+import 'package:flutter/material.dart';
+
+import '../../util/dimentions.dart';
+
+class PopularFoodDetail extends StatelessWidget {
+  const PopularFoodDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              height: Dimensions.popularFoodImgSize,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/image/food0.jpg"))),
+            ),
+          ),
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
